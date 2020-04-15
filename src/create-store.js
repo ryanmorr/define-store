da4630dfba2dab6a39bf2fcba6f18004cea00623
Ryan Morr <rm.morr@gmail.com>
2020-04-15 14:56:23 -0400

    Add a variety of improvements

--- src/create-store.js
+++ src/create-store.js
@@ -1,34 +1,31 @@
 export default function createStore(setup) {
-    let value = null;
-    const subscribers = [];
-    const get = () => value;
-    const set = (...args) => {
-        value = args[0];
-        emit(...args);
-        return value;
-    };
-    const subscribe = (subscriber, immediate = false) => {
-        if (!subscribers.includes(subscriber)) {
-            subscribers.push(subscriber);
-            if (immediate === true) {
-                subscriber(value);
+    return (...args) => {
+        let value;
+        const subscribers = [];
+        const get = () => value;
+        const set = (...args) => {
+            value = args[0];
+            emit(...args);
+            return value;
+        };
+        const subscribe = (callback) => {
+            if (!subscribers.includes(callback)) {
+                subscribers.push(callback);
+                callback(value);
+                return () => {
+                    const index = subscribers.indexOf(callback);
+                    if (index !== -1) {
+                        subscribers.splice(index, 1);
+                    }
+                };
             }
-            return () => {
-                const index = subscribers.indexOf(subscriber);
-                if (index !== -1) {
-                    subscribers.splice(index, 1);
-                }
-            };
-        }
+        };
+        const emit = (...args) => {
+            subscribers.slice().forEach((subscriber) => subscriber(...args));
+        };
+        const constructor = setup(get, set, subscribe, subscribers);
+        const store = constructor(...args);
+        store.subscribe = subscribe;
+        return store;
     };
-    const emit = (...args) => {
-        subscribers.slice().forEach((subscriber) => subscriber(...args));
-    };
-    const constructor = setup(get, set, subscribe, subscribers);
-    const callback = (...args) => {
-        const inner = constructor(...args);
-        inner.subscribe = subscribe;
-        return inner;
-    };
-    return callback;
 }
--- test/specs/create-store.js
+++ test/specs/create-store.js
@@ -4,12 +4,13 @@ import createStore from '../../src/create-store';
 
 describe('create-store', () => {
     it('should create a basic function-based observable store', () => {
-        const store = createStore((get, set, subscribe, subscribers) => () => {
+        const store = createStore((get, set, subscribe, subscribers) => (value) => {
             expect(get).to.be.a('function');
             expect(set).to.be.a('function');
             expect(subscribe).to.be.a('function');
             expect(subscribers).to.be.an('array');
 
+            set(value);
             return (...args) => {
                 if (args.length === 1) {
                     set(args[0]);
@@ -18,28 +19,278 @@ describe('create-store', () => {
             };
         });
 
-        const value = store();
+        const value = store('foo');
         expect(value).to.be.a('function');
-        expect(value()).to.equal(null);
-        expect(value('foo')).to.equal('foo');
         expect(value()).to.equal('foo');
+        expect(value('bar')).to.equal('bar');
+        expect(value()).to.equal('bar');
+    });
 
-        const spy = sinon.spy();
-        expect(value.subscribe).to.be.a('function');
-        const unsubscribe = value.subscribe(spy);
-        expect(spy.callCount).to.equal(0);
+    it('should return a default inner value of undefined', () => {
+        const store = createStore((get, set) => () => {
+            return (...args) => {
+                if (args.length > 0) {
+                    set(...args);
+                }
+                return get();
+            };
+        });
+
+        const value = store();
+        expect(value()).to.equal(undefined);
+    });
+
+    it('should support subscribers to be called immediately and when the value changes', () => {
+        const store = createStore((get, set) => (value) => {
+            set(value);
+            return (...args) => {
+                if (args.length > 0) {
+                    set(args[0]);
+                }
+                return get();
+            };
+        });
+
+        const value = store('foo');
+
+        const spy1 = sinon.spy();
+        const spy2 = sinon.spy();
+
+        value.subscribe(spy1);
+        expect(spy1.callCount).to.equal(1);
+        expect(spy1.args[0][0]).to.equal('foo');
+
+        value.subscribe(spy2);
+        expect(spy2.callCount).to.equal(1);
+        expect(spy2.args[0][0]).to.equal('foo');
 
         value('bar');
-        expect(value()).to.equal('bar');
-        expect(spy.callCount).to.equal(1);
-        expect(spy.args[0][0]).to.equal('bar');
 
-        expect(unsubscribe).to.be.a('function');
-        unsubscribe();
+        expect(spy1.callCount).to.equal(2);
+        expect(spy1.args[1][0]).to.equal('bar');
+        expect(spy2.callCount).to.equal(2);
+        expect(spy2.args[1][0]).to.equal('bar');
+        expect(spy1.calledBefore(spy2)).to.equal(true);
+        
+        value('baz');
+
+        expect(spy1.callCount).to.equal(3);
+        expect(spy1.args[2][0]).to.equal('baz');
+        expect(spy2.callCount).to.equal(3);
+        expect(spy2.args[2][0]).to.equal('baz');
+    });
+
+    it('should not allow the same function to subscribe more than once', () => {
+        const store = createStore((get, set) => (value) => {
+            set(value);
+            return (...args) => {
+                if (args.length > 0) {
+                    set(args[0]);
+                }
+                return get();
+            };
+        });
+
+        const value = store('foo');
+
+        const spy = sinon.spy();
+
+        value.subscribe(spy);
+        value.subscribe(spy);
+        value.subscribe(spy);
+
+        expect(spy.callCount).to.equal(1);
 
         value('baz');
-        expect(value()).to.equal('baz');
+
+        expect(spy.callCount).to.equal(2);
+    });
+
+    it('should remove a subscriber', () => {
+        const store = createStore((get, set) => (value) => {
+            set(value);
+            return (...args) => {
+                if (args.length > 0) {
+                    set(args[0]);
+                }
+                return get();
+            };
+        });
+
+        const value = store('foo');
+
+        const spy = sinon.spy();
+
+        const unsubscribe = value.subscribe(spy);
+
         expect(spy.callCount).to.equal(1);
+
+        value('bar');
+
+        expect(spy.callCount).to.equal(2);
+        
+        unsubscribe();
+        value('baz');
+
+        expect(spy.callCount).to.equal(2);
+    });
+
+    it('should allow subscribers to remove themselves without disrupting others', () => {
+        const store = createStore((get, set) => (value) => {
+            set(value);
+            return (...args) => {
+                if (args.length > 0) {
+                    set(args[0]);
+                }
+                return get();
+            };
+        });
+
+        const value = store('foo');
+
+        let unsubscribe;
+        let doUnsubscribe = false;
+
+        const spy1 = sinon.spy();
+        const spy2 = sinon.spy(() => {
+            if (doUnsubscribe) {
+                unsubscribe();
+            }
+        });
+        const spy3 = sinon.spy();
+
+        value.subscribe(spy1);
+        unsubscribe = value.subscribe(spy2);
+        value.subscribe(spy3);
+
+        expect(spy1.callCount).to.equal(1);
+        expect(spy2.callCount).to.equal(1);
+        expect(spy3.callCount).to.equal(1);
+
+        value('bar');
+
+        expect(spy1.callCount).to.equal(2);
+        expect(spy2.callCount).to.equal(2);
+        expect(spy3.callCount).to.equal(2);
+        
+        doUnsubscribe = true;
+        value('baz');
+
+        expect(spy1.callCount).to.equal(3);
+        expect(spy2.callCount).to.equal(3);
+        expect(spy3.callCount).to.equal(3);
+
+        value('qux');
+
+        expect(spy1.callCount).to.equal(4);
+        expect(spy2.callCount).to.equal(3);
+        expect(spy3.callCount).to.equal(4);
+    });
+
+    it('should return the new value within a subscriber', () => {
+        const store = createStore((get, set) => (value) => {
+            set(value);
+            return (...args) => {
+                if (args.length > 0) {
+                    set(args[0]);
+                }
+                return get();
+            };
+        });
+
+        const value = store('foo');
+
+        const spy = sinon.spy(() => {
+            if (spy.callCount === 1) {
+                expect(value()).to.equal('foo');
+            } else {
+                expect(value()).to.equal('bar');
+            }
+        });
+
+        value.subscribe(spy);
+        expect(spy.callCount).to.equal(1);
+
+        value('bar');
+        expect(spy.callCount).to.equal(2);
+    });
+
+    it('should support multiple args to set and subscribers', () => {
+        const store = createStore((get, set) => () => {
+            return (...args) => {
+                if (args.length > 0) {
+                    set(...args);
+                }
+                return get();
+            };
+        });
+
+        const value = store();
+        expect(value()).to.equal(undefined);
+        expect(value(1, 2, 3, 4, 5)).to.equal(1);
+        expect(value()).to.equal(1);
+
+        const spy = sinon.spy();
+        value.subscribe(spy);
+        expect(spy.callCount).to.equal(1);
+        expect(spy.args[0].length).to.equal(1);
+        expect(spy.args[0][0]).to.equal(1);
+
+        value('foo', 'bar', 'baz');
+        expect(value()).to.equal('foo');
+        expect(spy.callCount).to.equal(2);
+        expect(spy.args[1].length).to.equal(3);
+        expect(spy.args[1][0]).to.equal('foo');
+        expect(spy.args[1][1]).to.equal('bar');
+        expect(spy.args[1][2]).to.equal('baz');
+    });
+
+    it('should support multiple instances of a store', () => {
+        const store = createStore((get, set) => (value) => {
+            set(value);
+            return (...args) => {
+                if (args.length > 0) {
+                    set(...args);
+                }
+                return get();
+            };
+        });
+
+        const value1 = store('foo');
+        const value2 = store(1);
+
+        expect(value1()).to.equal('foo');
+        expect(value2()).to.equal(1);
+
+        const spy1 = sinon.spy();
+        const unsubscribe1 = value1.subscribe(spy1);
+        expect(spy1.callCount).to.equal(1);
+
+        const spy2 = sinon.spy();
+        value2.subscribe(spy2);
+        expect(spy2.callCount).to.equal(1);
+
+        value1('bar');
+        expect(value1()).to.equal('bar');
+        expect(spy1.callCount).to.equal(2);
+        expect(spy1.args[1][0]).to.equal('bar');
+        expect(value2()).to.equal(1);
+        expect(spy2.callCount).to.equal(1);
+
+        value2(2);
+        expect(value2()).to.equal(2);
+        expect(spy2.callCount).to.equal(2);
+        expect(spy2.args[1][0]).to.equal(2);
+        expect(value1()).to.equal('bar');
+        expect(spy1.callCount).to.equal(2);
+
+        unsubscribe1();
+
+        value1('baz');
+        expect(value1()).to.equal('baz');
+        expect(spy1.callCount).to.equal(2);
+        expect(value2()).to.equal(2);
+        expect(spy2.callCount).to.equal(2);
     });
 
     it('should create a basic object-based observable store', () => {
@@ -54,76 +305,25 @@ describe('create-store', () => {
         expect(value).to.be.an('object');
         expect(value.get).to.be.a('function');
         expect(value.set).to.be.a('function');
-        expect(value.get()).to.equal(null);
+        expect(value.get()).to.equal(undefined);
         expect(value.set('foo')).to.equal('foo');
         expect(value.get()).to.equal('foo');
 
         const spy = sinon.spy();
         expect(value.subscribe).to.be.a('function');
         const unsubscribe = value.subscribe(spy);
-        expect(spy.callCount).to.equal(0);
+        expect(spy.callCount).to.equal(1);
 
         value.set('bar');
         expect(value.get()).to.equal('bar');
-        expect(spy.callCount).to.equal(1);
-        expect(spy.args[0][0]).to.equal('bar');
+        expect(spy.callCount).to.equal(2);
+        expect(spy.args[1][0]).to.equal('bar');
 
         expect(unsubscribe).to.be.a('function');
         unsubscribe();
 
         value.set('baz');
         expect(value.get()).to.equal('baz');
-        expect(spy.callCount).to.equal(1);
-    });
-
-    it('should return a default inner value of null', () => {
-        const store = createStore((get, set) => () => {
-            return (...args) => {
-                if (args.length > 0) {
-                    set(...args);
-                }
-                return get();
-            };
-        });
-
-        const value = store();
-        expect(value()).to.equal(null);
-    });
-
-    it('should create a store with an initial value', () => {
-        const store = createStore((get, set) => (value) => {
-            set(value);
-            return get;
-        });
-
-        const value = store('foo');
-        expect(value()).to.equal('foo');
-    });
-
-    it('should support multiple args to set and subscribers', () => {
-        const store = createStore((get, set) => (value = null) => {
-            set(value);
-            return (...args) => {
-                if (args.length > 0) {
-                    set(...args);
-                }
-                return get();
-            };
-        });
-
-        const value = store();
-        expect(value()).to.equal(null);
-        expect(value(1, 2, 3, 4, 5)).to.equal(1);
-        expect(value()).to.equal(1);
-
-        const spy = sinon.spy();
-        value.subscribe(spy);
-
-        value('foo', 'bar', 'baz');
-        expect(value()).to.equal('foo');
-        expect(spy.callCount).to.equal(1);
-        expect(spy.args[0][0]).to.equal('foo');
-        expect(spy.args[0][1]).to.equal('bar');
-        expect(spy.args[0][2]).to.equal('baz');
+        expect(spy.callCount).to.equal(2);
     });
 });
